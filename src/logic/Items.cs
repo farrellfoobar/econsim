a275d742b08d01d3b7761f02ff8b5c6732f2ee2d
Ava Farrell <avafarrell415@gmail.com>
2026-05-11 15:51:53 -0700

    tinyies refactor to make next change clearer

--- src/logic/Items.cs
+++ src/logic/Items.cs
@@ -9,11 +9,14 @@ public class Items
     public static readonly List<ItemType> ALL_DESIRED_ITEMS = new List<ItemType> { ItemType.GRAIN, ItemType.FISH };
     
     public static Dictionary<ItemType, CoinAmount> BASE_PRICE = new Dictionary<ItemType, CoinAmount> {
+        { ItemType.FISH, CoinAmount.Silver(4) },
+        
         { ItemType.GRAIN, CoinAmount.Silver(1) },
+        { ItemType.BEER, CoinAmount.Silver(3) },
+        
         { ItemType.WOOD, CoinAmount.Silver(5) },
         { ItemType.FURNITURE, CoinAmount.Silver(20) },
-        { ItemType.FISH, CoinAmount.Silver(4) },
-        { ItemType.BEER, CoinAmount.Silver(3) },
+        
         { ItemType.SILVER_ORE, CoinAmount.Silver(1000) },
         { ItemType.JEWELRY, CoinAmount.Silver(3000) },
     };
