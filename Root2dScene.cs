3d4ec4478b4c77b56b318edd6f479d144c7769e0
Ava Farrell <avafarrell415@gmail.com>
2026-05-17 14:07:24 -0700

    removed entrypoint dir since we cant move root2d scene without messing with godot editor, added graceful exit if tests fail

--- Root2dScene.cs
+++ Root2dScene.cs
@@ -7,7 +7,7 @@ namespace EconSim;
 
 public partial class Root2dScene : Node2D
 {
-	private bool isTest = false;
+	private bool isRunningTests = false;
 	
 	private Simulation simulation;
 	private SimulationRenderer renderer;
@@ -15,10 +15,14 @@ public partial class Root2dScene : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		isTest = "true".Equals(System.Environment.GetEnvironmentVariable("--test"));
-		if (isTest) {
-			TestExecutor.Main();
-			GetTree().Quit();
+		isRunningTests = "true".Equals(System.Environment.GetEnvironmentVariable("--test"));
+		if (isRunningTests) {
+			try {
+				TestExecutor.Main();
+			}
+			finally {
+				GetTree().Quit();
+			}
 		} else {
 			simulation = new Simulation();
 			renderer = new SimulationRenderer(this, simulation);
@@ -28,7 +32,7 @@ public partial class Root2dScene : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (!isTest) {
+		if (!isRunningTests) {
 			simulation.DoTurn();
 			renderer.RenderSimulation();
 			OS.DelayMsec(1000);
